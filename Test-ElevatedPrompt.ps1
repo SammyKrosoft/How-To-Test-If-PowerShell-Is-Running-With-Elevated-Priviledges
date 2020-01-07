41a862b58f5dd800ad1a6994528d1e4d599ce3f7
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2020-01-07 08:58:47 -0500

    Create Test-ElevatedPrompt.ps1

---
+++ Test-ElevatedPrompt.ps1
@@ -0,0 +1,21 @@
+function Test-IsAdmin {
+    try {
+        $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+        $principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
+        return $principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )
+    } catch {
+        throw "Failed to determine if the current user has elevated privileges. The error was: '{0}'." -f $_
+    }
+
+    <#
+        .SYNOPSIS
+            Checks if the current Powershell instance is running with elevated privileges or not.
+        .EXAMPLE
+            PS C:\> Test-IsAdmin
+        .OUTPUTS
+            System.Boolean
+                True if the current Powershell is elevated, false if not.
+         .LINK
+            Credits : https://github.com/aarismendi/ps-scripts/blob/master/Modules/Security/Security.psm1
+    #>
+}
